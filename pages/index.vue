0589c831f6db0e3028d7677b688c65400205f0fa
MohammadRanjbar <mmohammadrranjbar@gmail.com>
2024-08-31 16:30:18 +0330

    fix: overflow issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <UContainer class="h-dvh flex flex-col items-center justify-center">
-    <UCard class="md:size-[500px] m-4 size-full">
+    <UCard class="md:size-[500px] m-4 size-full overflow-auto">
       <UForm @submit="onSubmit" :state :schema class="space-y-3">
         <UFormGroup label="Who is Joking" required name="name">
           <UInput v-model="state.name" placeholder="Enter Name Here" />
